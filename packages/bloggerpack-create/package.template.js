cd578ef4fdd5c23e225c6719a4be40152c495dff
Igoy Nawamreh <igoynawamreh@gmail.com>
2021-03-17 01:50:38 +0700

    test: fix missing semicolon

--- packages/bloggerpack-create/package.template.js
+++ packages/bloggerpack-create/package.template.js
@@ -5,7 +5,7 @@ const packageTemplate = (content, args) => {
   let key2 = {
     name: args.projectName,
     ...key
-  }
+  };
   key = key2;
 
   let newKey = JSON.stringify(key, null, 2).trim() + '\n';
