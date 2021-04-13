54d273d2ceb79e84462bba6288eb6de8fb5fb2cf
Igoy Nawamreh <igoynawamreh@gmail.com>
2021-04-14 01:46:46 +0700

    chore: add eslint-config-bloggerpack

---
+++ packages/eslint-config-bloggerpack/README.md
@@ -0,0 +1,20 @@
+# eslint-config-bloggerpack
+
+> Standard eslint config for Bloggerpack.
+
+## Installation
+
+```bash
+$ npm install eslint-config-bloggerpack --save-dev
+```
+
+## Usage
+
+`.eslintrc.json`:
+
+```json
+{
+  "root": true,
+  "extends": "eslint-config-bloggerpack"
+}
+```
---
+++ packages/eslint-config-bloggerpack/index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:unicorn/recommended"
+  ],
+  rules: {
+    "capitalized-comments": "off",
+    "indent": [
+      "error",
+      2,
+      {
+        "MemberExpression": "off",
+        "SwitchCase": 1
+      }
+    ],
+    "max-params": [
+      "warn",
+      5
+    ],
+    "multiline-ternary": [
+      "error",
+      "always-multiline"
+    ],
+    "new-cap": "off",
+    "no-console": "off",
+    "object-curly-spacing": [
+      "error",
+      "always"
+    ],
+    "prefer-named-capture-group": "off",
+    "semi": [
+      "error",
+      "always"
+    ],
+    "import/no-unresolved": "off",
+    "unicorn/consistent-function-scoping": "off",
+    "unicorn/explicit-length-check": "off",
+    "unicorn/import-index": "off",
+    "unicorn/import-style": "off",
+    "unicorn/no-fn-reference-in-iterator": "off",
+    "unicorn/no-for-loop": "off",
+    "unicorn/no-null": "off",
+    "unicorn/no-unused-properties": "error",
+    "unicorn/no-useless-undefined": "off",
+    "unicorn/prefer-dataset": "off",
+    "unicorn/prefer-node-append": "off",
+    "unicorn/prefer-node-remove": "off",
+    "unicorn/prefer-optional-catch-binding": "off",
+    "unicorn/prefer-query-selector": "off",
+    "unicorn/prevent-abbreviations": "off",
+    "unicorn/filename-case": "off"
+  }
+};
